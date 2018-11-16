7dbf95a47d145993e54fbdb4e8902999089536ef
Anton Boltach <boltach.anton@gmail.com>
2018-11-16 09:57:53 +0300

    Fixed some problems

--- ruleOfFiveExample/src/ruleOfFiveExample.cpp
+++ ruleOfFiveExample/src/ruleOfFiveExample.cpp
@@ -1,8 +1,72 @@
-#include "ruleOfFiveExample.h"
+﻿#include "ruleOfFiveExample.h"
 
-int main() {
-    customArray a(9);
-    a.fillArrayValues(10);
-    customArray b(a);
-    b.printArray();
+//Constructor
+explicit CustomArray::CustomArray(size_t size) : data_(new int[size]), size_(size) {
+	std::cout << __FUNCTION__ << std::endl << std::endl;
+	fillArrayValues(0);
+}
+
+// Destructor
+CustomArray::~CustomArray() {
+	std::cout << __FUNCTION__ << std::endl << std::endl;
+	delete[] data_;
+}
+
+// Сopy сonstructor
+CustomArray::CustomArray(CustomArray const &c) : size_(c.size_), data_(new int[c.size_]) {
+	std::cout << __FUNCTION__ << std::endl << std::endl;
+	std::copy(c.data_, c.data_ + c.size_, data_);
+}
+
+// Move constructor
+CustomArray::CustomArray(CustomArray &&c) : data_(c.data_), size_(c.size_) {
+	std::cout << __FUNCTION__ << std::endl << std::endl;
+
+	c.data_ = nullptr;
+	c.size_ = 0;
+}
+
+// Copy assignment operator
+CustomArray& CustomArray::operator=(CustomArray const &c) {
+	std::cout << __FUNCTION__ << std::endl << std::endl;
+	if (this != &c) {
+		// Free the existing resource.
+		delete[] data_;
+
+		size_ = c.size_;
+		data_ = new int[size_];
+		std::copy(c.data_, c.data_ + this->size_, this->data_);
+	}
+
+	return *this;
+}
+
+// Move assignment operator
+CustomArray& CustomArray::operator=(CustomArray &&c) {
+	std::cout << __FUNCTION__ << std::endl << std::endl;
+	assert(this != &c);
+
+	// Free the existing resource.
+	delete[] data_;
+
+	data_ = c.data_;
+	size_ = c.size_;
+
+	c.data_ = nullptr;
+	c.size_ = 0;
+
+	return *this;
+}
+
+void CustomArray::fillArrayValues(const int &value) {
+	std::cout << __FUNCTION__ << std::endl << std::endl;
+	std::fill(data_, data_ + size_, value);
+}
+
+std::ostream& operator<<(std::ostream &os, const CustomArray &c) {
+	os << __FUNCTION__ << std::endl;
+	std::ostream_iterator<int> out_it(os, " ");
+	std::copy(c.data_, c.data_ + c.size_, out_it);
+	os << std::endl << std::endl;
+	return os;
 }
--- ruleOfFiveExample/src/ruleOfFiveExample.h
+++ ruleOfFiveExample/src/ruleOfFiveExample.h
@@ -1,81 +1,32 @@
 #include <algorithm>
+#include <iterator>
 #include <iostream> 
 #include <cassert>
 
-using std::cout;
-using std::endl;
-
-class customArray {
+class CustomArray {
 private:
     int *data_;
     size_t size_;
 public: 
-    explicit customArray(size_t size) : data_(new int[size]), size_(size) {
-        cout << __FUNCTION__ << endl << endl;
-        fillArrayValues(0);
-    }
+	//Constructor
+	explicit CustomArray(size_t size);
 
     // Destructor
-    ~customArray() {
-        cout << __FUNCTION__ << endl << endl;
-        delete[] data_; 
-    }
+	~CustomArray();
 
     // Сopy сonstructor
-    customArray(customArray const & c) : size_(c.size_), data_(new int[c.size_]) {
-        cout << __FUNCTION__ << endl << endl;
-        std::copy(c.data_, c.data_ + c.size_, data_);
-    }
+	CustomArray(CustomArray const &c);
 
     // Move constructor
-    customArray(customArray &&c) : data_(c.data_), size_(c.size_) {
-        cout << __FUNCTION__ << endl << endl;
-    
-        c.data_ = nullptr;
-        c.size_ = 0;
-    }
+	CustomArray(CustomArray &&c);
 
     // Copy assignment operator
-    customArray &operator=(customArray const &c) {
-        cout << __FUNCTION__ << endl << endl;
-        assert(this != &c);
+	CustomArray& operator=(CustomArray const &c);
 
-        // Free the existing resource.
-        delete[] data_;
+    // Move assignment operator
+	CustomArray &operator=(CustomArray &&c);
 
-        size_ = c.size_;
-        data_ = new int[size_];
-        std::copy(c.data_, c.data_ + this->size_, this->data_);
+	friend std::ostream& operator<<(std::ostream &os, const CustomArray &c);
 
-        return *this;
-    }
-
-     // Move assignment operator
-    customArray &operator=(customArray &&c) {
-        cout << __FUNCTION__ << endl << endl;
-        assert(this != &c);
-
-        // Free the existing resource.
-        delete[] data_;
-
-        data_ = c.data_;
-        size_ = c.size_;
-
-        c.data_ = nullptr;
-        c.size_ = 0;
-
-        return *this;
-    }
-
-    void printArray() {
-        cout << __FUNCTION__ << endl;
-        std::ostream_iterator<int> out_it(cout, " ");
-        std::copy(data_, data_ + size_, out_it);
-        cout << endl << endl;
-    }
-
-    void fillArrayValues(const int &value) { 
-        cout << __FUNCTION__ << endl << endl;
-        std::fill(data_, data_ + size_, value); 
-    }
+	void fillArrayValues(const int &value);
 };
