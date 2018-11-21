9f0fde9e237127dc809a6e63b38d5761925ceaad
Ashes17b <Ashes17b@gmail.com>
2018-11-21 23:52:01 +0300

    joinForString boost_unit_test

--- joinForString/src/testJoin.cpp
+++ joinForString/src/testJoin.cpp
@@ -1,22 +1,34 @@
 #include "join.h"
 
 #include <iostream>
-#include <cassert>
 #include <vector>
 #include <list>
 
-int main() {
+#define BOOST_TEST_MODULE test_filter_iterator
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(test_suite1)
+
+BOOST_AUTO_TEST_CASE(test_join_for_list) {
     std::list<std::string> l;
     l.push_back("Hello");
     l.push_back("World!");
 
+    auto joined = join(l, ", ");
+
+    BOOST_CHECK_EQUAL(joined, "Hello, World!");
+}
+
+BOOST_AUTO_TEST_CASE(test_join_for_vector)
+{
     std::vector<std::string> v;
     v.push_back("Hello");
     v.push_back("World!");
 
-    auto joinedVector = join(v, ", ");
-    auto joinedList = join(l, ", ");
-    assert(joinedVector == joinedList);
+    auto joined = join(v, ", ");
 
-    return 0;
+    BOOST_CHECK_EQUAL(joined, "Hello, World!");
 }
+
+BOOST_AUTO_TEST_SUITE_END()
