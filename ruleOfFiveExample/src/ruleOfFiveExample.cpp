9e9dce644824498d545e35ba47aebaf71374cad5
Ashes17b <Ashes17b@gmail.com>
2018-11-28 20:54:58 +0300

    Minor bug fixes

--- ruleOfFiveExample/src/ruleOfFiveExample.cpp
+++ ruleOfFiveExample/src/ruleOfFiveExample.cpp
@@ -1,20 +1,23 @@
 ﻿#include "ruleOfFiveExample.h"
 
-//Constructor
+// Constructor
 explicit CustomArray::CustomArray(size_t size) : data_(new int[size]), size_(size) {
 	std::cout << __FUNCTION__ << std::endl << std::endl;
+	
 	fillArrayValues(0);
 }
 
 // Destructor
 CustomArray::~CustomArray() {
 	std::cout << __FUNCTION__ << std::endl << std::endl;
+
 	delete[] data_;
 }
 
 // Сopy сonstructor
 CustomArray::CustomArray(CustomArray const &c) : size_(c.size_), data_(new int[c.size_]) {
 	std::cout << __FUNCTION__ << std::endl << std::endl;
+
 	std::copy(c.data_, c.data_ + c.size_, data_);
 }
 
@@ -29,6 +32,7 @@ CustomArray::CustomArray(CustomArray &&c) : data_(c.data_), size_(c.size_) {
 // Copy assignment operator
 CustomArray& CustomArray::operator=(CustomArray const &c) {
 	std::cout << __FUNCTION__ << std::endl << std::endl;
+
 	if (this != &c) {
 		// Free the existing resource.
 		delete[] data_;
@@ -44,6 +48,7 @@ CustomArray& CustomArray::operator=(CustomArray const &c) {
 // Move assignment operator
 CustomArray& CustomArray::operator=(CustomArray &&c) {
 	std::cout << __FUNCTION__ << std::endl << std::endl;
+
 	assert(this != &c);
 
 	// Free the existing resource.
@@ -60,11 +65,13 @@ CustomArray& CustomArray::operator=(CustomArray &&c) {
 
 void CustomArray::fillArrayValues(const int &value) {
 	std::cout << __FUNCTION__ << std::endl << std::endl;
+
 	std::fill(data_, data_ + size_, value);
 }
 
 std::ostream& operator<<(std::ostream &os, const CustomArray &c) {
 	os << __FUNCTION__ << std::endl;
+
 	std::ostream_iterator<int> out_it(os, " ");
 	std::copy(c.data_, c.data_ + c.size_, out_it);
 	os << std::endl << std::endl;
