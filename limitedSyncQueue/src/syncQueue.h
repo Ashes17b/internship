1a23172e4cecdc39e27c74a322e2657983c0196a
Ashes17b <Ashes17b@gmail.com>
2018-12-01 10:35:41 +0300

    Add some features in syncQueue.h

--- limitedSyncQueue/src/syncQueue.h
+++ limitedSyncQueue/src/syncQueue.h
@@ -13,33 +13,78 @@ class LimitedBuffer
     std::condition_variable cv_;
 
     std::size_t size_;
-  public:
-    LimitedBuffer(size_t size) : size_(size) {}
+    unsigned int n_clients_;
 
-    LimitedBuffer(const LimitedBuffer &) = delete;
-    LimitedBuffer &operator=(const LimitedBuffer &) = delete;
+    bool is_stopping_;
+
+  public:
+    LimitedBuffer(size_t size) : size_(size), is_stopping_(false), n_clients_(0) {}
+
+    ~LimitedBuffer() {
+        {
+            std::unique_lock<std::mutex> lock(mx_);
+            is_stopping_ = true;
+        }
+
+        cv_.notify_all();
+
+        {
+            std::unique_lock<std::mutex> lock(mx_);
+            cv_.wait(lock, [this]() { return n_clients_ == 0; });
+        }
+    }
 
     T pop()
     {
-        std::unique_lock<std::mutex> lock(mx_);
+        bool check_stopping_;
+        T val;
 
-        cv_.wait(lock, [this](){ return queue_.size() > 0; });
+        {
+            std::unique_lock<std::mutex> lock(mx_);
+            ++n_clients_;
 
-        auto val = queue_.front();
-        queue_.pop();
+            cv_.wait(lock, [this]() { return is_stopping_ || !queue_.empty(); });
+
+            if (!is_stopping_) {
+                val = queue_.front();
+                queue_.pop();
+            }
+
+            --n_clients_;
+            check_stopping_ = is_stopping_;
+        }
+
+        cv_.notify_all();
+
+        if (check_stopping_)
+            throw; //An empty queue is terminating
 
         return val;
     }
 
     void push(const T &item)
     {   
+        bool check_stopping_;
+
         {
             std::unique_lock<std::mutex> lock(mx_);
-            cv_.wait(lock, [this](){ return queue_.size() < size_; });
+            ++n_clients_;
 
-            queue_.push(item);
+            cv_.wait(lock, [this](){ return is_stopping_ || queue_.size() < size_; });
+
+            if (!is_stopping_) queue_.push(item);
+
+            --n_clients_;
+
+            check_stopping_ = is_stopping_;
         }
+
+        if (check_stopping_)
+            throw; //An queue is terminating
 
         cv_.notify_all();
     }
+
+    LimitedBuffer(const LimitedBuffer &) = delete;
+    LimitedBuffer &operator=(const LimitedBuffer &) = delete;
 };
